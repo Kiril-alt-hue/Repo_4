98921d587a5105231e1f3a1c8e389db1ac0a762b
Kiril Batrak <kirabatrak@gmail.com>
2025-02-04 09:37:15 +0200

    modif utils.py

--- utils.py
+++ utils.py
@@ -4,3 +4,10 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
+
+
+def gcd(a, b):
+    """Обчислення найбільшого спільного дільника (НСД) двох чисел."""
+    while b:
+        a, b = b, a % b
+    return a
