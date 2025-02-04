2721e0bd481c293450f42470e94803f0b7c5cb92
Maksim <MaksimUluanovOfficial@gmail.com>
2025-02-04 09:42:56 +0200

    Подільність на 5

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
-
 def factorial(n):
     """Обчислення факторіалу числа n (n!)."""
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
+
+def five(n):
+    if n <= 0:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
